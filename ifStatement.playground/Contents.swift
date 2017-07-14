a579e6d3dc5ba4cf612ae7703caafeec41b28537
Farouk <faroue10@gmail.com>
2017-07-14 17:13:36 +0100

    My first comit

---
+++ ifStatement.playground/Contents.swift
@@ -0,0 +1,12 @@
+var birthYear = 1970
+
+if birthYear < 2000 {
+    
+    print("You're born in the 20th century!")
+    
+   } else {
+    
+        print("You're born in the 21st century!")
+    }
+    
+    
